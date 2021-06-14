550147dde55b4473f5e2a0ff8625b136efc2db9b
IoanLT <ioan.tranole@gmail.com>
2021-06-14 14:33:08 +0100

    initial project setup

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+
+ReactDOM.render((
+	<div className="xanda-react-trial">
+		<h1>Xanda React Trial</h1>
+		<p>Please read and follow the instructions in README.MD file.</p>
+		<p>Edit the js file in /js folder. Feel free to create additional files/components and structure them nicely.</p>
+	</div>
+), app);
