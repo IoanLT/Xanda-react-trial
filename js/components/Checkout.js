1d2ea38b79c1cd46657bc403b27708427285c90e
IoanLT <ioan.tranole@gmail.com>
2021-06-16 12:27:00 +0100

    adding basic styling to the checkout page

--- js/components/Checkout.js
+++ js/components/Checkout.js
@@ -1,39 +1,49 @@
-import React from 'react';
-import BasketListItem from './BasketListItem';
+import React from "react";
+import BasketListItem from "./BasketListItem";
+import logo from "../../assets/logo.png";
 
 const Checkout = ({ addToBasket, goToProducts, removeFromCart }) => {
 	let price = 0;
 
 	return (
 		<div className="checkout-wrapper">
-			<h2>This is the Checkout page</h2>
-			<button className="go-back-btn" onClick={goToProducts}>Go back</button>
-			{addToBasket.length > 0 ? (
-				addToBasket.map((product, index) => {
-					price += product.price;
-
-					return (
-						<BasketListItem
-							product={product}
-							key={product.id}
-							index={index}
-							addToBasket={addToBasket}
-							removeFromCart={removeFromCart}
-						/>
-					);
-				})
-			) : (
-				<h5>There are no items in the basket</h5>
-			)}
-
-			{addToBasket.length > 0 && (
-				<div className="basket-total">
-					<h4>Total</h4>
-					<h4>{price}</h4>
+			<section className="checkout-logo-section">
+				<div>
+					<img src={`${logo}`} alt="game logo" />
 				</div>
-			)}
+			</section>
+
+			<section className="checkout-products">
+				<button className="go-back-btn" onClick={goToProducts}>
+					Go back
+				</button>
+				{addToBasket.length > 0 ? (
+					addToBasket.map((product, index) => {
+						price += product.price;
+
+						return (
+							<BasketListItem
+								product={product}
+								key={product.id}
+								index={index}
+								addToBasket={addToBasket}
+								removeFromCart={removeFromCart}
+							/>
+						);
+					})
+				) : (
+					<h5>There are no items in the basket</h5>
+				)}
+
+				{addToBasket.length > 0 && (
+					<div className="basket-total">
+						<h4>Total</h4>
+						<h4>{price}</h4>
+					</div>
+				)}
+			</section>
 		</div>
 	);
 };
 
-export default Checkout
+export default Checkout;
